04bafbb8999e2c756b20c6c3cc12e44d001d8c38
Galán <antoniogalanG15@gmail.com>
2022-10-23 13:07:53 +0200

    Comentarios traducidos, 'print_coords()' renombrada

--- ex00/rush00.c
+++ ex00/rush00.c
@@ -6,21 +6,21 @@
 /*   By: antgalan <antgalan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 08:57:06 by antgalan          #+#    #+#             */
-/*   Updated: 2022/10/23 09:28:27 by antgalan         ###   ########.fr       */
+/*   Updated: 2022/10/23 13:04:08 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
 /**
- * @brief	Imprime un carácter según la figura descrita.
+ * @brief	Prints a character according to the figure described.
  * 
- * @param a		Coordenada 'a' del carácter a mostrar.
- * @param b 	Coordenada 'b' del carácter a mostrar.
- * @param max_x	Máxima coordenada 'x' (ancho) de la figura.
- * @param max_y Máxima coordenada 'y' (alto) de la figura.
-*/
-void	print_coords(int a, int b, int max_x, int max_y)
+ * @param a 	Coordinate 'a' of the character to display.
+ * @param b 	Coordinate 'b' of the character to display.
+ * @param max_x Maximum 'x' coordinate (width) of the figure.
+ * @param max_y Maximum 'y' coordinate (height) of the figure.
+ */
+void	select_char(int a, int b, int max_x, int max_y)
 {
 	max_x--;
 	max_y--;
@@ -35,10 +35,10 @@ void	print_coords(int a, int b, int max_x, int max_y)
 }
 
 /**
- * @brief   Muestra la figura pedida según el RUSH 00.
+ * @brief	Displays the requested figure according to RUSH 00.
  *
- * @param x		Ancho total de la figura.
- * @param y		Alto total de la figura.
+ * @param x		Total width of the shape.
+ * @param y 	Total height of the shape.
  */
 void	rush(int x, int y)
 {
@@ -53,7 +53,7 @@ void	rush(int x, int y)
 			width = 0;
 			while (width < x)
 			{
-				print_coords(width, height, x, y);
+				select_char(width, height, x, y);
 				width++;
 			}
 			ft_putchar('\n');
--- ex00/rush03.c
+++ ex00/rush03.c
@@ -6,21 +6,21 @@
 /*   By: antgalan <antgalan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 12:20:25 by antgalan          #+#    #+#             */
-/*   Updated: 2022/10/23 09:40:51 by antgalan         ###   ########.fr       */
+/*   Updated: 2022/10/23 13:00:54 by antgalan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
 /**
- * @brief	Imprime un carácter según la figura descrita.
+ * @brief	Prints a character according to the figure described.
  * 
- * @param a		Coordenada 'a' del carácter a mostrar.
- * @param b 	Coordenada 'b' del carácter a mostrar.
- * @param max_x	Máxima coordenada 'x' (ancho) de la figura.
- * @param max_y Máxima coordenada 'y' (alto) de la figura.
-*/
-void	print_coords(int a, int b, int max_x, int max_y)
+ * @param a 	Coordinate 'a' of the character to display.
+ * @param b 	Coordinate 'b' of the character to display.
+ * @param max_x Maximum 'x' coordinate (width) of the figure.
+ * @param max_y Maximum 'y' coordinate (height) of the figure.
+ */
+void	select_char(int a, int b, int max_x, int max_y)
 {
 	max_x--;
 	max_y--;
@@ -35,10 +35,10 @@ void	print_coords(int a, int b, int max_x, int max_y)
 }
 
 /**
- * @brief   Muestra la figura pedida según el RUSH 03.
+ * @brief	Displays the requested figure according to RUSH 03.
  *
- * @param x		Ancho total de la figura.
- * @param y		Alto total de la figura.
+ * @param x		Total width of the shape.
+ * @param y 	Total height of the shape.
  */
 void	rush(int x, int y)
 {
@@ -53,7 +53,7 @@ void	rush(int x, int y)
 			width = 0;
 			while (width < x)
 			{
-				print_coords(width, height, x, y);
+				select_char(width, height, x, y);
 				width++;
 			}
 			ft_putchar('\n');
